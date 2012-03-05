ee300fd2572f3226af5e472858373d51f24a449a
Justin Rocha <xenith@xenith.org>
2012-03-05 09:09:46 -0800

    Merge in some useful code for fabfile.py and update wsgi.py to work well for virtualenv.

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -1,12 +1,14 @@
-from fabric.api import cd, run, env, local, sudo
+from fabric.api import cd, run, env, local, sudo, require
+from fabric.operations import _prefix_commands, _prefix_env_vars
 from lib.fabric_helpers import *
 import os
 import string
 
 
-env.hosts = ['{{ project_name }}']
+env.hosts = ['{{ project_name }}.example.com']
 env.code_dir = '/srv/www/{{ project_name }}'
 env.virtualenv = '/srv/www/{{ project_name }}/.virtualenv'
+env.code_repo = 'git@github.com:user/{{project_name}}.git'
 env.django_settings_module = '{{ project_name }}.settings'
 
 
@@ -26,28 +28,88 @@ def uname():
 
 
 def push():
-    "Push new code and pull on all hosts"
+    """ Push new code and pull on all hosts """
     local('git push origin master')
     with cd(env.code_dir):
         run('git pull origin master')
 
 
 def update_requirements():
-    "Update requirements in the virtualenv."
+    """ Update requirements in the virtualenv. """
     run("%s/bin/pip install -r %s/requirements/prod.txt" % (env.virtualenv, env.code_dir))
 
 
 def migrate(app=None):
-    """Run the migrate task
-    Usage: fab migrate:app_name"""
+    """
+    Run the migrate task
+    Usage: fab migrate:app_name
+    """
     if app:
         run("source %s/bin/activate; django-admin.py migrate %s --settings=%s" % (env.virtualenv, app, env.django_settings_module))
     else:
         run("source %s/bin/activate; django-admin.py migrate --settings=%s" % (env.virtualenv, env.django_settings_module))
 
 
+def version():
+    """ Show last commit to the deployed repo. """
+    with cd(env.code_dir):
+        run('git log -1')
+
+
+def restart():
+    """ Restart the wsgi process """
+    with cd(env.code_dir):
+        run("touch %s/{{ project_name }}/wsgi.py" % env.code_dir)
+
+
+def ve_run(cmd):
+    """
+    Helper function.
+    Runs a command using the virtualenv environment
+    """
+    require('root')
+    return sshagent_run('source %s/bin/activate; %s' % (env.virtualenv, cmd))
+
+
+def sshagent_run(cmd):
+    """
+    Helper function.
+    Runs a command with SSH agent forwarding enabled.
+
+    Note:: Fabric (and paramiko) can't forward your SSH agent.
+    This helper uses your system's ssh to do so.
+    """
+    # Handle context manager modifications
+    wrapped_cmd = _prefix_commands(_prefix_env_vars(cmd), 'remote')
+    try:
+        host, port = env.host_string.split(':')
+        return local(
+            "ssh -p %s -A %s@%s '%s'" % (port, env.user, host, wrapped_cmd)
+        )
+    except ValueError:
+        return local(
+            "ssh -A %s@%s '%s'" % (env.user, env.host_string, wrapped_cmd)
+        )
+
+
 def deploy():
+    """ Update the remote deployment, update the virtualenv, perform any
+    pending migrations, then restart the wsgi process """
     push()
     update_requirements()
     migrate()
-    run("touch %s/wsgi/django.wsgi" % env.code_dir)
+    restart()
+
+
+def clone():
+    """ Clone the repository for the first time """
+    with cd(env.code_dir):
+        run('git clone %s .' % (env.code_repo))
+
+
+def bootstrap():
+    """ Bootstrap the initial deploy environment, then deploy """
+    run('mkdir %s' % (env.code_dir))
+    run('virtualenv %s' % (env.virtualenv))
+    clone()
+    deploy()
--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -14,6 +14,40 @@ framework.
 
 """
 import os
+import sys
+import site
+import subprocess
+
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../")
+
+# Add the virtualenv packages to the site directory. This uses the technique
+# described at http://code.google.com/p/modwsgi/wiki/VirtualEnvironments
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path)
+
+# Get the path to the env's site-packages directory
+site_packages = subprocess.check_output([
+                    os.path.join(REPO_PATH, '.virtualenv/bin/python'),
+                    '-c',
+                    'from distutils.sysconfig import get_python_lib;'
+                    'print get_python_lib(),'
+]).strip()
+
+# Add the virtualenv site-packages to the site packages
+site.addsitedir(site_packages)
+
+# Reorder sys.path so the new directories are at the front.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+# Add the app code to the path
+sys.path.append(PROJECT_ROOT)
 
 os.environ['CELERY_LOADER'] = 'django'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
