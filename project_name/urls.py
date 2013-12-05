c864d546ef765bec7dac938679dd941b9897edda
Justin Seabrook-Rocha <xenith@xenith.org>
2013-12-04 17:10:51 -0800

    Serving staticfiles like this in development isn't actually needed, since we use django.contrib.staticfiles.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,5 @@
 """ Default urlconf for {{ project_name }} """
 
-from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 admin.autodiscover()
@@ -19,13 +18,3 @@ urlpatterns = patterns('',
     url(r'', include('base.urls')),
 )
 
-## In DEBUG mode, serve media files through Django.
-if settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
-    # Remove leading and trailing slashes so the regex matches.
-    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
-    urlpatterns += patterns('',
-        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
-    )
