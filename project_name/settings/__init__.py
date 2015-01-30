432ff5fda3ca1490765fa502b851d898e44aa300
Torbjörn Klatt <torbjoernk@users.noreply.github.com>
2015-01-30 08:52:11 +0100

    little fix for Python 3

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -3,7 +3,7 @@
 from .base import *
 try:
     from .local import *
-except ImportError, exc:
+except ImportError as exc:
     exc.args = tuple(
         ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
     raise exc
